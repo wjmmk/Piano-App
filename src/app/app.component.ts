054ad45c7061f9a95e531cc5b56186b5fa0e4484
William Mosquera <wjmmk3@gmail.com>
2022-01-10 11:02:24 -0500

    Initial load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
   title = 'PianoApp';
+
+  context= new AudioContext();
+ 
+  jsNota(frecuencia:number){
+    var osc = this.context.createOscillator();
+    var gain = this.context.createGain();
+        osc.connect(gain);
+        osc.type="sine";
+        osc.frequency.value=frecuencia;
+        gain.connect(this.context.destination);
+        osc.start(0);
+        gain.gain.exponentialRampToValueAtTime(0.00001,this.context.currentTime +1.5);
+  }
 }
